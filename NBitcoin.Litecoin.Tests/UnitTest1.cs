3b2ab29c5ec1206cd092ffd15abce52b2e0850a0
NicolasDorier <nicolas.dorier@gmail.com>
2017-05-02 03:08:21 +0900

    add litecoin networks

---
+++ NBitcoin.Litecoin.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NBitcoin.Protocol;
+using System;
+using Xunit;
+
+namespace NBitcoin.Litecoin.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+			LitecoinNetworks.Register();
+        }
+    }
+}

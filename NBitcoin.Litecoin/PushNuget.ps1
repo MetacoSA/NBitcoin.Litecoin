1c2076e80b6ab1a835e194898c4f0be20fe97945
nicolas.dorier <nicolas.dorier@gmail.com>
2017-12-31 01:58:35 +0900

    fix pushnuget

--- NBitcoin.Litecoin/PushNuget.ps1
+++ NBitcoin.Litecoin/PushNuget.ps1
@@ -1,14 +1,6 @@
-del bin\Release\*.nupkg
-&("D:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe") "NBitcoin.Litecoin.csproj" /t:pack -p:Configuration=Release
-
-cd bin\Release
-
-# nuGet pack NBitcoin.nuspec
-
-forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
-(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
-$ver = $Matches.Item(0)
+rm "bin\release\" -Recurse -Force
+dotnet pack --configuration Release
+dotnet nuget push "bin\Release\" --source "https://api.nuget.org/v3/index.json"
+$ver = ((ls .\bin\release -File)[0].Name -replace '([^\.\d]*\.)+(\d+(\.\d+){1,3}).*', '$2')
 git tag -a "v$ver" -m "$ver"
 git push --tags
-
-cd ..\..
